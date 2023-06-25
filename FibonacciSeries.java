86a197222593211223a8e38ca120483382d00996
mukesharvind <137684181+mukesharvind@users.noreply.github.com>
2023-06-25 05:58:08 -0300

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+
+		int firstNum = 0;
+		int secNum = 1;
+		int sum = 0;
+		System.out.println(firstNum);
+		for (int i = 0; i <= 11; i++) {
+			sum = firstNum + secNum;
+			firstNum = secNum;
+			secNum = sum;
+
+			System.out.println("The fibbonacci sereis is :" + sum);
+		}
+
+	}
+
+}
